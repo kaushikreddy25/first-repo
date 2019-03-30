a4c901c3817146412cd58f47f3cbf881f61faf63
guruk <guruk@KaushikPC>
2019-03-30 11:00:30 +0530

    File Handling 1st Commit

--- MyPython/SelfLearning/fileHandlingDemo.py
+++ MyPython/SelfLearning/fileHandlingDemo.py
@@ -1,6 +1,10 @@
 f = open('myData.txt','r')
+import os
+print(os.getcwd())
+
 
 print(f.read())
+print(f.readlines())
 
 f1 = open('copiedData.txt','w')
 
@@ -15,8 +19,8 @@ for i in f:
 
 f2 = open('Barca.jpg','rb')
 
-for j in f2:
-    print(j)
+#for j in f2:
+    #print(j)
     
 f2 = open('Barca.jpg','rb')
 f3 = open('copiedPic.jpg','wb')
@@ -24,8 +28,23 @@ f3 = open('copiedPic.jpg','wb')
 for k in f2:
     f3.write(k)
 
+import shelve
 
+sf = shelve.open('shelvefile')
+sf['Football Clubs'] = ['Barcelona','Arsenal','Girona', 'Sevilla']
 
+sf = shelve.open('shelvefile')
+print(sf['Football Clubs'])
+sf = shelve.open('shelvefile')
+print('Keys are----')
+print(list(sf.keys()))
+sf = shelve.open('shelvefile')
+
+print('Values are---')
+print(list(sf.values()))
+sf.close()
+
+#https://1drv.ms/u/s!Amv7MQc0Jhpisl7CDBRf8InwMfDY
 
 '''
     r = read mode
---
+++ MyPython/SelfLearning/fileHandlingDemo2.py
@@ -0,0 +1,49 @@
+'''
+Created on 29-Mar-2019
+
+@author: guruk
+'''
+import os
+
+print(os.path.join('folder1','folder2 in folder1','folder3 in folder2','samsung.jpeg -in folder3-'))
+
+print(os.getcwd())
+
+os.chdir('W:\\Python\\Automate the Boring Stuff with Python Programming - Udemy')
+
+print(os.getcwd())
+
+print('Absolute Path----')
+file1 = os.path.abspath('Files1.png')
+print(file1)
+
+print('Dir NAme ----')
+print(os.path.dirname(file1))
+print('Base name----')
+print(os.path.basename(file1))
+
+print('To check if a file exists on hard drive---')
+print(os.path.exists(file1))
+print(os.path.exists('W:\\Python\\Programs\\Helloworld.py'))
+print('To get file size in bytes---')
+print(os.path.getsize('W:\\Jobs\\CV.docx'))
+
+print('To list all files in a folder---')
+print(os.listdir('W:\\Jobs'))
+
+print('--------------------------')
+# TODO - To find the total size of all files in a folder.
+
+totalsize = 0
+
+for file in os.listdir('W:\\Jobs'):
+    if os.path.isfile(os.path.join('W:\\Jobs',file)):
+        totalsize = totalsize + os.path.getsize(os.path.join('W:\\Jobs',file))
+    else:
+        continue
+
+print("Total size is: ",totalsize)
+
+
+#https://1drv.ms/u/s!Amv7MQc0JhpislyDudLxv4s12iYB
+#https://1drv.ms/u/s!Amv7MQc0Jhpisl0PLJoPxyucdChA
